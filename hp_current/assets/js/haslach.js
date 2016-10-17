e573837a5a0c1220cf1e8e9bd27c64f4267e6564
markusmo <markus.mo@gmx.net>
2016-10-17 20:30:20 +0200

    init commit

---
+++ hp_current/assets/js/haslach.js
@@ -0,0 +1,5 @@
+function openGoogleMapsOnHaslach()
+{
+	window.open('https://www.google.com/maps/place/Restaurant+Haslach/@47.3892361,9.7375533,17z/data=!3m1!4b1!4m5!3m4!1s0x479b6b2826c6fe4b:0x7377df3b8ac0e080!8m2!3d47.3892361!4d9.739742'); 
+	//return false;
+}
